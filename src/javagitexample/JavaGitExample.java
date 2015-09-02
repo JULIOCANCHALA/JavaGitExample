58befa6ec10f5b08043bcc88c42cb039304ba8e9
Julio <Julio@JulioPC.lan>
2015-09-02 11:29:29 -0500

    Version 1.0 Version de  prueba de conexion a HitGub

---
+++ src/javagitexample/JavaGitExample.java
@@ -0,0 +1,18 @@
+package javagitexample;
+
+public class JavaGitExample {
+    
+    public void imprimir()
+    {
+        System.out.println("Control de Versiones con GitHub");
+    }
+
+    public static void main(String[] args) {
+        JavaGitExample ejemplo;
+        ejemplo=new JavaGitExample();
+        
+        ejemplo.imprimir();
+
+    }
+    
+}
